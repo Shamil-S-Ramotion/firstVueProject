dab334dbe1f7bc24da5485713521b7f1bbbcd17f
work_user <work_user@MacBook-Pro-Shamil.local>
2022-09-14 13:07:12 +0300

    initialize project

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="navbar">
+    <div @click="$router.push('/')">Inst</div>
+    <div class="navbar__btns">
+      <my-button @click="$router.push('/posts')">Posts</my-button>
+      <my-button @click="$router.push('/about')" style="margin-left: 20px"
+        >About</my-button
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+import MyButton from "@/components/UI/MyButton";
+export default {
+  components: { MyButton },
+};
+</script>
+
+<style scoped>
+.navbar {
+  height: 50px;
+  background: gray;
+  box-shadow: 2px 2px 4px gray;
+  display: flex;
+  align-items: center;
+  padding: 0 15px;
+}
+.navbar__btns {
+  margin-left: auto;
+}
+</style>
---
+++ src/pages/About.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <div>
+     <h1>About page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  /* eslint-disable */
+}
+</script>
+
+<style scoped>
+
+</style>
+/* eslint-disable */
